bcffbffcf4f8a080f68c515f81400ced5d867f54
uzulla <uzulla@himitsukichi.com>
2022-03-17 19:40:04 +0900

    add bee page

--- resources/views/b.blade.php
+++ resources/views/b.blade.php
@@ -7,6 +7,6 @@
         <title>MyApp</title>
     </head>
     <body>
-        <h1>Hello world</h1>
+        <h1>Bee</h1>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/b', function () {
+    return view('b');
+});
