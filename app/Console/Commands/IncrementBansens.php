2c0a0369e56620a7925ecc281764a611e972d158
uzulla <uzulla@himitsukichi.com>
2022-03-18 21:33:18 +0900

    add increment tool

---
+++ app/Console/Commands/IncrementBansens.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Bansen;
+use Illuminate\Console\Command;
+
+class IncrementBansens extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bansen:increment';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $bansen = new Bansen();
+
+        $bansen->save();
+
+        return 0;
+    }
+}
