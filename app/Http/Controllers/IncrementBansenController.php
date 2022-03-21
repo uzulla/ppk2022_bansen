5c49c8039f2635bb4aa6447c72cdec41e99a2314
uzulla <uzulla@himitsukichi.com>
2022-03-22 01:06:50 +0900

    touch up.

--- app/Http/Controllers/IncrementBansenController.php
+++ app/Http/Controllers/IncrementBansenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Bansen;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Log\Logger;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class IncrementBansenController extends Controller
@@ -12,9 +13,11 @@ class IncrementBansenController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): SymfonyResponse
+    public function __invoke(Request $request, Logger $logger): SymfonyResponse
     {
         $bansen = Bansen::insertOne();
+
+        $logger->notice("latest bansen no: {$bansen->id}");
 
         $next_url = route("latest_bansen");
 
