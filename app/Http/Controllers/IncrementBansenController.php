771684bf6f87c8b5688bd9fea78e7f61eb7097f1
uzulla <uzulla@himitsukichi.com>
2022-03-22 20:37:47 +0900

    refactoring error handling. add log.

--- app/Http/Controllers/IncrementBansenController.php
+++ app/Http/Controllers/IncrementBansenController.php
@@ -6,7 +6,9 @@ use App\Models\Bansen;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Log\Logger;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 class IncrementBansenController extends Controller
 {
@@ -15,7 +17,12 @@ class IncrementBansenController extends Controller
      */
     public function __invoke(Request $request, Logger $logger): SymfonyResponse
     {
-        $bansen = Bansen::insertOne();
+        try {
+            $bansen = Bansen::insertOne();
+        } catch (RuntimeException $e) {
+            $logger->error("fatal: insert bansen failed. {$e->getMessage()}", ['error' => $e]);
+            throw new ServiceUnavailableHttpException("fatal: insert bansen failed. {$e->getMessage()}");
+        }
 
         $logger->notice("latest bansen no: {$bansen->id}");
 
